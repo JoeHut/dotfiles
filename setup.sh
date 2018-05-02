8401bf2be3ca579134e10efc157ce006827cc352
Johannes Hutter <johannes.5494@gmail.com>
2018-05-02 21:56:12 +0200

    feat: make sure backup dir is not overwritten

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -x
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 CONFIG_DIR=$HOME/.config
+CONFIG_DIR_BAK=$CONFIG_DIR.bak
 
 if [ -d "$CONFIG_DIR" ]; then
   if [ -L "$CONFIG_DIR" ]; then
@@ -11,7 +12,12 @@ if [ -d "$CONFIG_DIR" ]; then
     echo ".config is just a symlink. Take care manually."
     exit -1
   else
-    cp -r $CONFIG_DIR $CONFIG_DIR.bak
+    if [ -d "$CONFIG_DIR_BAK" ]; then
+      echo "Backup of config directory already exists. Take care manually"
+      exit -1
+    fi
+
+    cp -r $CONFIG_DIR $CONFIG_DIR_BAK
   fi
 else
   mkdir -p $CONFIG_DIR
